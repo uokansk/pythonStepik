b32daa23aa40335067ed333b806992de7fde4b58
Aleksandr <uorkansk@yandex.ru>
2022-10-12 23:59:42 +0700

    найти счастливый билет

---
+++ task010Ticket.py
@@ -0,0 +1,20 @@
+# number = int(input())
+# a = number // 100000
+# b = number // 10000 % 10
+# c = number // 1000 % 10
+# d = number // 100 % 10
+# e = number // 10 % 10
+# f = number % 10
+# if a + b + c == d + e + f:
+#     print("Счастливый")
+# else:
+#     print("Обычный")
+
+
+a = input()
+if int(a[0]) + int(a[1]) + int(a[2]) == int(a[3]) + int(a[4]) + int(a[5]):
+
+    print('Счастливый')
+else:
+    print('Обычный')
+print(a[0], a[1], a[2], a[3], a[4], a[5], sep="\n")
