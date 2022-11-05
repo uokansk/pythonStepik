39719b6a4c46f8a1925b19b5be7441560c0eaee5
Aleksandr <uorkansk@yandex.ru>
2022-11-05 19:04:13 +0700

    сократил  и улучшил таблицу умножения

--- multiplicationTable.py
+++ multiplicationTable.py
@@ -2,5 +2,7 @@ numberHorizon = int(input())
 numberVertical = int(input())
 for i in range(1, numberHorizon+1):
     for j in range(1, numberVertical + 1):
-        print(i*j, end='\t')
+        if i <= j:
+            print(i*j, end='\t')
     print()
+    print(i+1, end='\t'*i)
